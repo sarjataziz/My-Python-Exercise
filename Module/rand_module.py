03a838296408b53a1993c078047f2acc90e955b5
sarjataziz <sarjat.aziz@gmail.com>
2022-08-31 00:18:46 +0600

    Delete Some Exercise

--- Module/rand_module.py
+++
@@ -1,4 +0,0 @@
-from random import choice as pick, randint as magic_number_chooser
-
-print(pick(["apple", "banana", "cherry", "durian"]))
-print(magic_number_chooser(1,100))
--- Module/random1.py
+++
@@ -1,3 +0,0 @@
-import random as rand
-
-print(rand.choice(["sda","ad","qe","f","rewfsd"]))
